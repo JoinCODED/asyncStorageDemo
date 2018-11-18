fef4a9c51307f11058ede6c66e9b4398991a89f9
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2018-11-19 02:23:04 +0300

    master

--- App.js
+++ App.js
@@ -1,57 +1,28 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  AsyncStorage,
-  TouchableHighlight
-} from "react-native";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      saved: null,
       unsaved: null
     };
   }
-  componentDidMount() {
-    AsyncStorage.getItem("saved").then(value => {
-      if (value) {
-        this.setState({ saved: value });
-      }
-    });
-  }
-  handlePress() {
-    if (this.state.unsaved) {
-      AsyncStorage.setItem("saved", this.state.unsaved);
-    } else {
-      AsyncStorage.removeItem("saved");
-    }
-    this.setState({ saved: this.state.unsaved, unsaved: null });
-  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Saved Value:</Text>
         <Text>{this.state.saved}</Text>
-        <TextInput
-          placeholder="Enter value"
-          style={styles.input}
-          value={this.state.unsaved}
-          onChangeText={value => this.setState({ unsaved: value })}
-        />
-        <TouchableHighlight
-          onPress={() => this.handlePress()}
-          style={styles.button}
-        >
+        <TextInput placeholder="Enter value" style={styles.input} />
+        <TouchableHighlight style={styles.button}>
           <Text>Submit</Text>
         </TouchableHighlight>
         <TouchableHighlight
           style={{
             ...styles.button,
-            backgroundColor: this.state.saved ? "red" : "green"
+            backgroundColor: "red"
           }}
         >
           <Text>Special Button</Text>
